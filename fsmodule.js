76a4b814fe860be9c06d4cabe05e86de944abeb2
meghraj thakre <102140277+meghrajthakre@users.noreply.github.com>
2023-11-24 11:16:52 +0530

    Add files via upload

---
+++ fsmodule.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+// fs.writeFile('demo.txt', "this is a demo file ", (err, data) => {
+//     console.log("file creation succefully completed");
+// })
+
+// fs.readFile('demo.txt', "utf-8", (err, data) => {
+//     console.log(data);
+// })
+
+// fs.appendFile('demo.txt', " additionally file i append ", (err, data) => {
+//     console.log("succefully created aapending");
+// })
+
+// fs.unlink('demo.txt', (err, data) => {
+//     console.log("file unlink or deleted successfully");
+// });
